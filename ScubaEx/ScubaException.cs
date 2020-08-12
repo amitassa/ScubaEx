1350a4dbd25ac36d0de816dff9cb9334ced77181
thelittlecitizen13 <amitassa9@gmail.com>
2020-08-12 18:10:00 +0300

    Scuba Exception class added

---
+++ ScubaEx/ScubaException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScubaEx
+{
+    public class ScubaException : Exception
+    {
+        public int studentNumber { get; }
+        public ScubaException(string exceptionMessage, int studentNumber) : base(exceptionMessage)
+        { }
+    }
+}
